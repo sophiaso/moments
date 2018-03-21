b37becff907a6dfabddff11994e5f5df5eaf0e88
weiping <wepa@microsoft.com>
2018-03-21 17:22:25 +0800

    redirect to login page

--- gateway-server/src/main/java/com/microsoft/azure/moments/config/OAuth2Config.java
+++ gateway-server/src/main/java/com/microsoft/azure/moments/config/OAuth2Config.java
@@ -9,9 +9,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @EnableOAuth2Client
 @Configuration
+@Controller
 public class OAuth2Config extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -23,5 +27,10 @@ public class OAuth2Config extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .formLogin().loginPage("/login").failureUrl("/login?error=true");
+    }
+
+    @GetMapping("/login")
+    public ModelAndView loginPage() {
+        return new ModelAndView("redirect:/login.html");
     }
 }
