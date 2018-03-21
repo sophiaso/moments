49adc84023bb9fe36bb6fb923514fd4c2bc9cefb
Weiping <pwp824@gmail.com>
2018-03-21 21:54:41 +0800

    add mvc configurer

---
+++ gateway-server/src/main/java/com/microsoft/azure/moments/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See LICENSE in the project root for
+ * license information.
+ */
+package com.microsoft.azure.moments.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/login").setViewName("forward:/login.html");
+    }
+}
