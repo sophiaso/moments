094668a3034bcaec81a5e465937495c85a0272c7
weiping <wepa@microsoft.com>
2018-03-26 15:38:19 +0800

    add config starter and read config from cloud config

--- auth-service/src/main/java/com/microsoft/azure/moments/AuthApplication.java
+++ auth-service/src/main/java/com/microsoft/azure/moments/AuthApplication.java
@@ -7,10 +7,10 @@ package com.microsoft.azure.moments;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
--- gateway-server/src/main/java/com/microsoft/azure/moments/config/WebSecurityConfig.java
+++ gateway-server/src/main/java/com/microsoft/azure/moments/config/WebSecurityConfig.java
@@ -16,7 +16,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/login**")
+                .antMatchers("/","/login**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
