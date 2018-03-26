3f9f439f0e0cdb2fabf30f717479a93c19f1cce3
weiping <wepa@microsoft.com>
2018-03-26 16:21:16 +0800

    use zuul to enable gateway redirection

--- gateway-server/src/main/java/com/microsoft/azure/moments/GatewayApplication.java
+++ gateway-server/src/main/java/com/microsoft/azure/moments/GatewayApplication.java
@@ -8,11 +8,12 @@ package com.microsoft.azure.moments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-//@EnableOAuth2Client
+@EnableZuulProxy
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
