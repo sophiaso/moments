0ef4020f93d97d4e5d7087a4bc45f4110989246a
weiping <wepa@microsoft.com>
2018-03-30 13:45:53 +0800

    add user info to the header nav

--- gateway-server/src/main/resources/static/src/components/Navigator.vue
+++ gateway-server/src/main/resources/static/src/components/Navigator.vue
@@ -1,19 +1,45 @@
 <template>
     <div>
-        <b-nav>
-            <b-nav-item active>Active</b-nav-item>
-            <b-nav-item>Link</b-nav-item>
-        </b-nav>
+        <b-navbar toggleable="md" type="dark" variant="info">
+            <b-collapse is-nav id="nav_collapse" v-if="hasLoggedIn">
+                <b-navbar-nav class="ml-auto">
+                    <b-nav-item-dropdown right>
+                        <template slot="button-content">
+                            {{ username }}
+                        </template>
+                        <b-dropdown-item href="#" v-on:click="logout">Signout</b-dropdown-item>
+                    </b-nav-item-dropdown>
+                </b-navbar-nav>
+            </b-collapse>
+            <b-navbar-nav class="ml-auto" v-else>
+                <b-nav-item href="/login">Sign in</b-nav-item>
+            </b-navbar-nav>
+        </b-navbar>
     </div>
 </template>
 <script>
-    import bNav from 'bootstrap-vue/es/components/nav/nav';
-    import bNavItem from 'bootstrap-vue/es/components/nav/nav-item';
+    import Vue from 'vue'
+    import {Collapse, Nav, Navbar} from 'bootstrap-vue/es/components';
+
+    Vue.use(Collapse);
+    Vue.use(Nav);
+    Vue.use(Navbar);
 
     export default {
-        components: {
-            'b-nav': bNav,
-            'b-nav-item': bNavItem
+        data: function() {
+            return {
+                username: 'user_1'
+            }
+        },
+        methods: {
+            logout: function() {
+                this.username = '';
+            }
+        },
+        computed: {
+            hasLoggedIn: function() {
+                return this.username && this.username.replace(/\s/g, '').length !== 0;
+            }
         }
     };
 </script>
