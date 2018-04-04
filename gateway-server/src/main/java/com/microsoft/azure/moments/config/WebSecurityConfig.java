acc2f1b4ae892cbae237446a1739254cf12fae75
weiping <wepa@microsoft.com>
2018-04-04 10:28:50 +0800

    permit all /js requests

--- gateway-server/src/main/java/com/microsoft/azure/moments/config/WebSecurityConfig.java
+++ gateway-server/src/main/java/com/microsoft/azure/moments/config/WebSecurityConfig.java
@@ -25,7 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/dist/**").permitAll()
+                .antMatchers("/dist/**", "/js/**").permitAll()
                 .antMatchers("/", "/login**")
                 .permitAll()
                 .and()
