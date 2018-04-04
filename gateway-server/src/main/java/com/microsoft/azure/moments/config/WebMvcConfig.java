984ce7803694efbff71f428daca658dc7a8c1f70
weiping <wepa@microsoft.com>
2018-04-04 10:28:18 +0800

    add /dist to resource location

--- gateway-server/src/main/java/com/microsoft/azure/moments/config/WebMvcConfig.java
+++ gateway-server/src/main/java/com/microsoft/azure/moments/config/WebMvcConfig.java
@@ -7,14 +7,26 @@ package com.microsoft.azure.moments.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableOAuth2Client
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/static/dist/"};
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("forward:/index.html");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
 }
