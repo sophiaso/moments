a975db5bcafb57ac551a1e55f52feaa6482e42d5
weiping <wepa@microsoft.com>
2018-04-04 10:38:56 +0800

    replace string with variable

--- gateway-server/webpack.config.js
+++ gateway-server/webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: 'src/main/webapp/index.html'
+            template: webapp_dir + '/index.html'
         })
     ],
     resolve: {
