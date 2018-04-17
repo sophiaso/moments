cf6912d568659dadc19cb7ddc66d7e9cde636554
weiping <wepa@microsoft.com>
2018-04-17 11:01:45 +0800

    remove zuul

--- gateway-server/src/main/java/com/microsoft/azure/moments/GatewayApplication.java
+++ gateway-server/src/main/java/com/microsoft/azure/moments/GatewayApplication.java
@@ -8,12 +8,9 @@ package com.microsoft.azure.moments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.web.reactive.config.EnableWebFlux;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulProxy
 public class GatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
