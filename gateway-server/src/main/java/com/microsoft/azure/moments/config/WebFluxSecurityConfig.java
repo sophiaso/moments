e561d9189de44d317bf032e8053579e29e96f430
weiping <wepa@microsoft.com>
2018-04-18 09:49:36 +0800

    allow same origin iframe to fix the x-iframe-options deny error

--- gateway-server/src/main/java/com/microsoft/azure/moments/config/WebFluxSecurityConfig.java
+++ gateway-server/src/main/java/com/microsoft/azure/moments/config/WebFluxSecurityConfig.java
@@ -6,17 +6,17 @@
 package com.microsoft.azure.moments.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.*;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.security.web.server.csrf.WebSessionServerCsrfTokenRepository;
+import org.springframework.security.web.server.header.XFrameOptionsServerHttpHeadersWriter;
 
 @EnableWebFluxSecurity
 public class WebFluxSecurityConfig {
     @Bean
     public SecurityWebFilterChain springWebFilterChain(ServerHttpSecurity http) throws Exception {
+        http.headers().frameOptions().mode(XFrameOptionsServerHttpHeadersWriter.Mode.SAMEORIGIN);
         return http
                 .authorizeExchange()
                 .pathMatchers("/dist/**", "/js/**").permitAll()
